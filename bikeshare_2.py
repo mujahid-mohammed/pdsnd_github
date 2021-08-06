b4e870de2f6bc54252bc3ad5063298405415648b
mujahid-mohammed <alsgmany9109@gmail.com>
2021-08-06 13:28:33 +0300

    refac: call the get data function.

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -254,6 +254,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
+        get_raw_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
